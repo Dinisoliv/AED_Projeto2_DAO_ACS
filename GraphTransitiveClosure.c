91f8d5f4342b7ff44e3209e75780a9f3c2cd91a4
AndreSilva-et <andre.castelhanos@ua.pt>
2024-12-27 16:55:40 +0000

    Added the GraphTransitiveClosure.c funtion

--- GraphTransitiveClosure.c
+++ GraphTransitiveClosure.c
@@ -6,10 +6,10 @@
 // GraphTransitiveClosure - Transitive Closure of a directed graph
 //
 
-// Student Name :
-// Student Number :
-// Student Name :
-// Student Number :
+// Student Name : André Silva
+// Student Number : 119480
+// Student Name : Dinis Oliveira
+// Student Number : 119193
 
 /*** COMPLETE THE GraphComputeTransitiveClosure FUNCTION ***/
 
@@ -31,7 +31,25 @@ Graph* GraphComputeTransitiveClosure(Graph* g) {
   assert(GraphIsDigraph(g));
   assert(GraphIsWeighted(g) == 0);
 
-  // COMPLETE THE CODE
+  //gets the number of vertices
+  unsigned int numVertices = GraphGetNumVertices(g);
 
-  return NULL;
+  //Creates a new Diagraph with the same number of vertices and not weighted
+  Graph* transitiveClosure = GraphCreate(numVertices, 1, 0); 
+
+  //iterates trough every vertice
+  for(unsigned int i = 0; i < numVertices; i++){
+    //executes the Bellman-Ford algorithm 
+    GraphBellmanFordAlg* bfa = GraphBellmanFordAlgExecute(g, i);
+    //iterates trough every vertice
+    for(unsigned int j = 0; j < numVertices; j++){
+      if(GraphBellmanFordAlgReached(bfa, j)/*  && j != i se não incluir laços */){
+        //Adds the transitive closure edge 
+        GraphAddEdge(transitiveClosure, i, j);
+      }
+    }
+    //frees the memory
+    GraphBellmanFordAlgDestroy(&bfa); 
+  }
+  return transitiveClosure;
 }
